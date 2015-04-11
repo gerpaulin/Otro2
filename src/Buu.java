127a68ecf999d824df5bb4a4733a0c0658320249
Germán <gerpaulin@gmail.com>
2015-04-11 14:48:14 -0300

    si

--- src/Buu.java
+++ src/Buu.java
@@ -1,8 +1,18 @@
 
 public class Buu {
 	
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		System.out.println("Buu y Buu");
+	}
+	
+	/**
+	 * 
+	 */
+	private void si(){
+		
 	}
 	
 }
