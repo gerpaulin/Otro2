d8f60c400015ea0fafbeaaeae453948d92613838
Germán <gerpaulin@gmail.com>
2015-04-11 14:43:16 -0300

    Update Buu.java

--- src/Buu.java
+++ src/Buu.java
@@ -1,13 +1,8 @@
 
 public class Buu {
-
-<<<<<<< HEAD
-	public static void main() {
-		System.out.println("Buu y Buu");
-=======
+	
 	public static void main(String[] args) {
-		System.out.println("Buu");
->>>>>>> origin/master
+		System.out.println("Buu y Buu");
 	}
 	
 }
